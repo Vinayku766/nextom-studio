14a54f65245166d50b64b9e85c75cdd94d583954
Vinay <vinaykumaryadav766@gmail.com>
2025-08-16 00:48:54 +0530

    changes on user photo and form check functionality

--- src/our-components/homepage/AboutSection.jsx
+++ src/our-components/homepage/AboutSection.jsx
@@ -4,16 +4,16 @@ import Image from 'next/image'
 const AboutSection = () => {
   return (
     <section id="about" className="bg-black text-white py-20 px-6">
-      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center gap-12">
+      <div className="max-w-6xl mx-auto flex flex-col lg:flex-row items-center gap-10">
         
         {/* Avatar */}
-        <div className="shrink-0 w-40 h-40 md:w-52 md:h-52 rounded-full overflow-hidden border-4 border-white/10">
+        <div className="shrink-0 w-50 h-50 md:w-52 md:h-52 rounded-full overflow-hidden border-4 border-white/10">
           <Image
             src="/User/Vipul-Dagar.png"
             alt="Vipul Dagar portrait"
-            width={208}
-            height={208}
-            className="w-full h-full object-cover"
+            width={240}
+            height={240}
+            className="w-full h-full object-cover scale-110"
           />
         </div>
 
