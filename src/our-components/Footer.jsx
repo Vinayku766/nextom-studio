736313d4b6d6429d510ef087ab4e75ce81795657
Vinay <vinaykumaryadav766@gmail.com>
2025-08-22 22:20:18 +0530

    fixed problem on footer

--- src/our-components/Footer.jsx
+++ src/our-components/Footer.jsx
@@ -55,18 +55,15 @@ const Footer = () => {
 
   return (
     <footer id="contact" className="relative bg-black text-white overflow-hidden px-5 py-5">
-      {/* Neon Aurora Background */}
 <div className="absolute inset-0 z-0 overflow-hidden">
   <div className="absolute inset-0 flex justify-between px-10">
-    {/* Left beams */}
+
     <div className="w-52 h-[500px] bg-gradient-to-t from-blue-800/90 via-cyan-700/80 to-transparent blur-[100px] animate-aurora-vertical"></div>
     <div className="w-40 h-[450px] bg-gradient-to-t from-indigo-700/90 via-purple-600/80 to-transparent blur-[90px] animate-aurora-fade [animation-delay:1s]"></div>
 
-    {/* Center beams */}
     <div className="w-56 h-[550px] bg-gradient-to-t from-cyan-600/90 via-blue-700/80 to-transparent blur-[120px] animate-aurora-wobble [animation-delay:2s]"></div>
     <div className="w-44 h-[400px] bg-gradient-to-t from-green-600/90 via-teal-500/80 to-transparent blur-[100px] animate-aurora-fade [animation-delay:3s]"></div>
 
-    {/* Right beams */}
     <div className="w-52 h-[480px] bg-gradient-to-t from-indigo-800/90 via-cyan-700/80 to-transparent blur-[110px] animate-aurora-vertical [animation-delay:4s]"></div>
     <div className="w-40 h-[420px] bg-gradient-to-t from-purple-700/90 via-blue-600/80 to-transparent blur-[100px] animate-aurora-wobble [animation-delay:5s]"></div>
   </div>
@@ -75,9 +72,7 @@ const Footer = () => {
       {/* Footer Content */}
       <div className="mx-auto px-6 max-w-7xl bg-white/5 backdrop-blur-md border border-white/10 rounded-xl shadow-lg py-4 relative z-10">
         <div className="flex flex-col sm:flex-row justify-between gap-8 py-3">
-          
-
-             {/* Right: Brand + Description + Contact */}
+        
           <div className="w-full sm:w-1/3 text-center sm:text-left space-y-4">
             <div className="flex justify-center sm:justify-start items-center gap-3 font-semibold">
               <Image
@@ -93,7 +88,6 @@ const Footer = () => {
               Nextom Studio is a creative video editing agency helping brands, creators, and businesses bring their stories to life. From cinematic edits to social media content, we deliver premium-quality videos with fast turnaround times and a collaborative approach.
             </p>
 
-            {/* Contact Icons */}
             <div className="flex justify-center sm:justify-start gap-4 text-white/70 text-lg">
               <Link href="mailto:nextomstudio@gmail.com" className="hover:text-blue-800 hover:scale-120 duration-300 transition">
                 <IoMailOpenOutline />
@@ -119,7 +113,7 @@ const Footer = () => {
             {sections.map((section, index) => (
               <div key={index} className="text-left">
                 {/* Desktop View */}
-                <h4 className="hidden sm:block text-lg font-bold font-semibold mb-3">{section.title}</h4>
+                <h4 className="hidden sm:block text-lg font-semibold mb-3">{section.title}</h4>
                 <ul className="hidden sm:flex flex-col space-y-2">
                   {section.links.map((link, i) => (
                     <li key={i}>
